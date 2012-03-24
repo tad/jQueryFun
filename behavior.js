ce80c7e52e9dd919ceca633f7b26c3ce82e0be7d
Terry Donaghe <Terry.Donaghe@gmail.com>
2012-03-24 15:39:32 -0700

    Nearly done with Chapter 2

--- behavior.js
+++ behavior.js
@@ -1,3 +1,24 @@
 $(document).ready(function () {
-	alert($('#celebs tbody tr:even').length + ' elements');
+	$('#no-script').remove();
+	$('#celebs tr:even').addClass('zebra');
+	$('<input type="button" value="Hide" id="toggleButton" />').insertAfter('#disclaimer');
+	$('#toggleButton').click(function () {
+		var that = this;
+		$('#disclaimer').slideToggle('slow', function () {
+			if($('#disclaimer').is(':visible')) {
+				$(that).val('Hide');			
+			} else {
+				$(that).val('Show');
+			}
+		});
+
+	});
+	$('#celebs tbody tr').hover(function() {
+		$(this).addClass('zebraHover');
+	}, function () {
+		$(this).removeClass('zebraHover');
+	});
+	$('#celebs tbody tr').click(function () {
+		$(this).toggleClass('zebraChosen');
+	})
 });
