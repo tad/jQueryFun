a9cb1ba9c0e35946ae1458700f4d513cdae25730
Terry Donaghe <Terry.Donaghe@gmail.com>
2012-03-25 11:53:22 -0700

    more stuff

--- behavior.js
+++ behavior.js
@@ -20,5 +20,37 @@ $(document).ready(function () {
 	});
 	$('#celebs tbody tr').click(function () {
 		$(this).toggleClass('zebraChosen');
-	})
+	});
+	
+	$('.spoiler').hide();
+	$('<input class="revealer" type="button" value="Tell Me!" />').insertBefore('.spoiler');
+	$('.revealer').click(function () {
+		$(this).hide();
+		$(this).next().fadeIn();
+	});
+	$('#navigation li').hover(function () {
+		$(this).animate({paddingLeft: '+=15px'}, 200);
+	}, function () {
+		$(this).animate({paddingLeft: '-=15px'}, 200);
+	});
+	
+	/*$('#disclaimer').animate({
+		opacity: 'hide',
+		height: 'hide'
+	}, 1000);*/
+	
+	$('#disclaimer').animate({'backgroundColor': '#ff9f5f'}, 1000);
+	
+	/*$('p').animate({
+		padding: '20px',
+		borderBottom: '3px solid #8f8f8f',
+		borderRight:'3px solid #bfbfbf'
+	}, 2000);*/
+	
+	$('p:first').toggle(function() {
+		$(this).animate({'height': '+=150px'}, 1000, 'easeOutElastic');
+	}, function() {
+		$(this).animate({'height': '-=150px'}, 1000, 'easeOutBack');
+	});
+	
 });
